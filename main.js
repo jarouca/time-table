76d59c22cdf8b22f374908f7ea47e4516a77d823
Jonathan Arouca <jonathanarouca@gmail.com>
2018-02-27 14:57:36 -0500

    add create row headers, rows, add unique id's to each table cell

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// assign variable
+// assign variables
 var table = '';
 var rows = 4;
 var cols = 24;
@@ -6,7 +6,7 @@ var row_headers = ['1:', '2:', '3:', '4:'];
 var col_headers = ['MID NIGHT', 'NOON'];
 var count = 11;
 
-// create column column headers
+// create column headers
 table += '<tr>';
   col_headers.forEach(function(header) {
     for(var col = 0; col < count + 1; col++) {
@@ -22,3 +22,25 @@ table += '</tr>'
 
 // create row headers
 // assign unique id and class to each div
+row_headers.forEach(function(header) {
+  table += '<tr>';
+  table += "<th class='row-header'>" + header + '</th>';
+    for(var c = 0; c < cols; c++) {
+      table += "<td class='time-table-cell'>" +
+      "<div class='first-quarter' id='" + header + " hour: " + c +
+      "'></div>" +
+      "<div class='second-quarter' id='" + header + " hour: " + c +
+      "'></div>" +
+      "<div class='third-quarter' id='" + header + " hour: " + c +
+      "'></div>" +
+      "<div class='fourth-quarter' id='" + header + " hour: " + c +
+      "'></div>" +
+      '</td>';
+      }
+    table += "<td class='row-total' id='" + header + "'></td>"
+  table += '</tr>';
+});
+
+var element = document.getElementById('table-wrapper')
+
+element.innerHTML += "<table class='time-table'>" + table + "</table>";
